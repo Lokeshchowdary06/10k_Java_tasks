5ed2039852464d91ce4b4c00cce8f8ce50e6028e
unknown <lokeshchowdary569@gmail.com>
2025-12-03 23:30:14 +0530

    Operators task on 03-12-25

---
+++ Task3.java
@@ -0,0 +1,19 @@
+package Operators;
+
+public class Task3 {
+	public static void bitwise(int x, int y)
+	{
+		System.out.println("Bitwise AND operation : "+(x&y));
+		System.out.println("Bitwise OR operation : "+(x|y));
+		System.out.println("Bitwise XOR operation : "+(x^y));
+		System.out.println("Bitwise LEFT SHIFT operation : "+(x<<y));
+		System.out.println("Bitwise RIGHT SHIFT operation : "+(x>>y));
+	}
+	public static void main(String[] args) {
+		int a=6;
+		int b=3;
+		bitwise(a, b);
+		
+	}
+
+}
---
+++ Task4.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class Task4 {
+	public static void updateScore(int score, int bonus)  {
+		System.out.println("Updated score after subtraction: " + (score-=bonus));
+		System.out.println("Updated score after addition: "+ (score+=bonus));
+		System.out.println("Updated score after division: "+ (score/=bonus));
+		System.out.println("Updated score after multiplication: "+ (score*=bonus));
+		System.out.println("Updated score after modulous: "+ (score%=bonus));
+	}
+	public static void main(String[] args) {
+		int score=69;
+		int bonus=5;
+		updateScore(score, bonus);
+	}
+
+}
