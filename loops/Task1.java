140d7c8d243e68643ef4246b0d74baeb776128d4
unknown <lokeshchowdary569@gmail.com>
2025-12-08 10:45:18 +0530

    Loops Statements Task

---
+++ loops/Task1.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class Task1 {
+	public static void main(String[] args) {
+		for(int i=1;i<11;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ loops/Task5.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class Task5 {
+	public static void main(String[] args) {
+		for(int i=5;i<51;i+=5)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
